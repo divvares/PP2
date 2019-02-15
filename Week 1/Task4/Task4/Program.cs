7097b8d5f715127b968045af92c1c74d509ca0c1
divvares <sultanelaman@gmail.com>
2019-02-16 00:51:44 +0600

    vse labki zagruzheni

---
+++ Week 1/Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //переводим массив из string'a массив из integer'a
+            for(int i = 1; i <= n; i++) // оба цикла нужны для того чтобы сделать тот самый треугольник из квадратных скобок и со звездочкой внутри
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("[*]"); //с помощью этого выводится элементы по столбичкам, а не по строчно
+                }
+                Console.WriteLine(); //выводим все на экран
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
