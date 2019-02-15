be96ef36e940ebca50fc5c2239e226a9d49e3659
divvares <sultanelaman@gmail.com>
2019-02-16 01:02:08 +0600

    added task 1

---
+++ Week 2/Task1/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool IsPalin(string str, int i) //рекурсивная функция для проверки палиндром или нет
+        {
+            if (str[i] != str[str.Length - 1 - i])
+                return false;
+            if (i == str.Length / 2)
+                return true;
+            return IsPalin(str, i + 1);
+        }
+        static void Main(string[] args)
+        {
+
+            StreamReader chitalka = new StreamReader("input.txt");
+            string s = chitalka.ReadToEnd(); // сделано для того чтобы прочитать input.txt файл с нужным нам input
+
+            if (IsPalin(s, 0)) // обращаемся к функции для проверки на палиндром
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            chitalka.Close();
+            Console.ReadKey();
+        }
+    }
+}
